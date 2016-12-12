6e0397350b562b7e9044a581fea42d10d66e71cb
serewann <serewann@gmail.com>
2016-12-13 01:46:22 +0200

    improvoe algo

--- assets/js/crypt.js
+++ assets/js/crypt.js
@@ -7,18 +7,39 @@ function encode(text)
     for (var  i = 0; i < text.length; i++) {
         var n = alphabet.indexOf(text[i].toUpperCase());
         result += "ф";
-        for (var j = 0; j < n; j++) {
+        for (var j = 0; j <= n; j++) {
             result += "ы";
+            if (j > 1 && (n - j) % 4 == 0) {
+                for (var z = 0; z < (n - j) / 4; z++) {
+                    result += "p";
+                }
+                break;
+            }
         }
         result += "р";
         if ((i+1) != text.length) result += '-';
     }
 
-    return result.replace(/-фр-/g, " ");
+    return result.replace(/-фр-/g, function(space) {
+        var random = Math.floor(Math.random() * 10);
+        if (random > 6) {
+            return " ";
+        } else if (random > 4) {
+            return " <3 ";
+        } else if (random > 2) {
+            return " ^^ ";
+        } else {
+            return " (ᵔᴥᵔ) ";
+        }
+    });
 }
 
 function decode(text) {
-    text = text.replace(/ф/g, "").replace(/р/g, "");
+    text = text.replace(/ф/g, "")
+        .replace(/р/g, "")
+        .replace(/p/g, "ыыыы")
+        .replace(/\<\3|\^\^|\(\ᵔ\ᴥ\ᵔ\)/g, "");
+
     var words = text.split(" ");
 
     var result = "";
@@ -28,7 +49,7 @@ function decode(text) {
 
         for (var j = 0; j < letters.length; j++) {
             var n = (letters[j].match(/ы/g) || []).length;
-            result += alphabet[n];
+            if (typeof  alphabet[n - 1] != "undefined") result += alphabet[n - 1];
         }
 
         result += " ";
